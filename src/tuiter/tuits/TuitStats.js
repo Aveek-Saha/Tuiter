a8f054bead97332ea68d30fef138c3dcf5b9b16d
Aveek Saha <aveek.s98@gmail.com>
2023-04-11 21:52:29 -0400

    Change links to buttons

--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -18,19 +18,18 @@ export default function TuitStats({ post }) {
         dispatch(likeTuit(post));
     };
     return (
-        <div className="row justify-content-around mb-2">
-            <a href="/#" className="col text-muted text-decoration-none">
+        <div className="row justify-content-center mb-2 mt-2">
+            <button className="btn btn-link col text-muted text-decoration-none">
                 <FontAwesomeIcon icon={faComment} />
                 <span> {post.replies}</span>
-            </a>
-            <a href="/#" className="col text-muted text-decoration-none">
+            </button>
+            <button className="btn btn-link col text-muted text-decoration-none">
                 <FontAwesomeIcon icon={faRetweet} />
                 <span> {post.retweets}</span>
-            </a>
-            <a
-                href="#"
-                className="col text-muted text-decoration-none"
+            </button>
+            <button
                 onClick={likeTuitClickHandler}
+                className="btn btn-link col text-muted text-decoration-none"
             >
                 {!post.liked && <FontAwesomeIcon icon={faHeartReg} />}
                 {post.liked && (
@@ -40,10 +39,10 @@ export default function TuitStats({ post }) {
                     />
                 )}
                 <span> {post.likes}</span>
-            </a>
-            <a href="/#" className="col text-muted text-decoration-none">
+            </button>
+            <button className="btn btn-link col text-muted text-decoration-none">
                 <FontAwesomeIcon icon={faArrowUpFromBracket} />
-            </a>
+            </button>
         </div>
     );
 }
