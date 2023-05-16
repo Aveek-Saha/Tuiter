4af89aa34e052e56df2cdccec72aca629fb9e40a
Aveek Saha <aveek.s98@gmail.com>
2023-05-16 19:20:24 -0400

    Use database server

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
+const API_BASE = process.env.REACT_APP_API_BASE_DATABASE || "http://localhost:4000/api";
 const TUITS_API = `${API_BASE}/tuits`;
 
 const currentUser = {
